06a8c24544118cb6208aa1b08dfa2d0e9343d86f
Joshua May <jmayprofessional@gmail.com>
2026-01-17 21:17:25 -0500

    workitem no dist

--- search-hawk/astro.config.mjs
+++ search-hawk/astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://jmayprofessional.github.io/search-hawk/', // your GitHub Pages URL
+  base: '/search-hawk/', // critical for routing & assets
   integrations: [tailwind(), mdx()],
 });
